2553302dd2befee582387ecfb65f88da9287cba4
Gabriel Vieira de Arimatéa <vieira.arimatea@gmail.com>
2020-10-14 13:16:35 -0300

    Create landing page and map page
    Need to fix map bug

---
+++ web/src/pages/OrphanagesMap.tsx
@@ -0,0 +1,43 @@
+import React from 'react';
+import {Link} from 'react-router-dom';
+import {FiPlus} from 'react-icons/fi';
+import {Map, TileLayer} from 'react-leaflet';
+
+import mapMarkerImg from '../images/map-marker.svg';
+
+import 'leaflet/dist/leaflet.css'
+import '../styles/pages/orphanages-map.css';
+
+function OrphanagesMap (){
+    return(
+        <div id="page-map">
+            <aside>
+                <header>
+                    <img src={mapMarkerImg} alt="Happy"/>
+
+                    <h2>Escolha um orfanato no mapa</h2>
+                    <p>Muitas crianças estão esperando a sua visita :)</p>
+                </header>
+
+                <footer>
+                    <strong>Distrito Federal</strong>
+                    <span>Brasíla</span>
+                </footer>
+            </aside>
+
+            <Map
+                center={[-15.7888065,-47.897539]}
+                zoom={13}
+                style={{ width: '100%', height: '100%' }}
+            >
+                <TileLayer url={`https://api.mapbox.com/styles/v1/mapbox/light-v10/tiles/256/{z}/{x}/{y}@2x?access_token=${process.env.REACT_APP_MAPBOX_TOLKEN}`} />
+            </Map>
+
+            <Link to="" className="create-orphanage">
+                <FiPlus size={32} color="#FFF"/>
+            </Link>
+        </div>
+    );
+}
+
+export default OrphanagesMap;
